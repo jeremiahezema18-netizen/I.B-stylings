968cf1591cef68caf2651c675de9d55ad84543c0
jeremiahezema18-netizen <jeremiahezema18@gmail.com>
2026-07-15 10:44:29 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,13 @@
+// ===============================
+// I.B. Stylings - Supabase Config
+// ===============================
+
+const SUPABASE_URL = "https://eravuxirymuooojyngjz.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_ZRFXaRtBDsXe9WY4iDDfUg_erIKsJDq";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
