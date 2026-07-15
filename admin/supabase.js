61e2fbc56e5566e527b4ab02df832e100b8945b0
jeremiahezema18-netizen <jeremiahezema18@gmail.com>
2026-07-15 12:39:03 +0100

    Update supabase.js

--- admin/supabase.js
+++ admin/supabase.js
@@ -1,14 +1,13 @@
 // supabase.js
-const supabaseUrl = "https://eravuxirymuooojyngjz.supabase.co";
-const supabaseKey = "sb_publishable_ZRFXaRtBDsXe9WY4iDDfUg_erIKsJDq";
+supabaseUrl = "https://eravuxirymuooojyngjz.supabase.co";
+supabaseKey = "sb_publishable_ZRFXaRtBDsXe9WY4iDDfUg_erIKsJDq";
 
-// 1. Create the client and assign it directly to a global 'supabase' variable
+// Create and bind globally
 supabase = window.supabase.createClient(
   supabaseUrl,
   supabaseKey
 );
 
-// 2. This ensures any script looking for window.supabase gets your configured client
 window.supabase = supabase;
 
 alert("Supabase.js loaded successfully");
